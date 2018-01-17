f72c2ff635ef268b5dac50e819940bd65a9f33a5
noorhaziq <code.noorhaziq@gmail.com>
2018-01-17 23:44:51 +0800

    basic data binding

---
+++ basic-data-binding.vue
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div id="root">
+		<input type="text" id="input" v-model="message">
+		<p>The value of the input is: {{ message }}</p>
+	</div>
+	
+	<script src="https://unpkg.com/vue@2.5.13/dist/vue.js"></script>
+	<script>
+		// vue@2.5.13
+		new Vue({
+			el: '#root',
+			data: {
+				message: 'Hello World',
+			}
+		});
+	</script>
+</body>
+</html>
